b41acc5d402654d6ab6079f6ee4dfc5c6bd87852
Taylor Treece <tayloratreece@gmail.com>
2021-01-05 16:49:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,25 +4,25 @@ def my_hash
     "profession" => "software developer"
   }
 
-  
+
 end
 
 
 def shipping_manifest
   the_manifest = {
     "whale bone corsets" => 5
-    "porcelain vases" => 2, 
+    "porcelain vases" => 2,
     "oil paintings" => 3
   }
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
 
 end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
 
@@ -32,8 +32,8 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
